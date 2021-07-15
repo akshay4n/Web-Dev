d23bc003808ede15687b53f4e1b2764da5a97f28
akshay4n <akshay2701n@gmail.com>
2021-07-15 15:04:56 +0530

    set-up json server and configured app.js

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,9 +1,64 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState,useEffect } from 'react'
+import AddTask from './components/AddTask'
+
 
 function App() {
+  const [showAddTask, setShowAddTask]=useState(false)
+  const[tasks, setTasks]=useState([])
+
+  useEffect(()=>{
+    const getTasks=async()=>{
+      const tasksFromServer=await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+
+  }, [])
+
+  //Fetch Tasks
+  const fetchTasks=async()=>{
+    const res=await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    // console.log(data)
+
+    return data
+  }
+
+//Add Task
+const addTask = (task)=>{
+  // console.log(task)
+  const id = Math.floor(Math.random()*10000)+1
+  // console.log(id)
+  const newTask={id,...task}
+  setTasks([...tasks,newTask])
+}
+
+//Delete Task
+const deleteTask=async (id)=>{
+  await fetch(`http://localhost:5000/tasks/${id}`,{
+    method:'DELETE'
+  })
+  setTasks(tasks.filter((task)=>task.id!==id))
+}
+
+//Toggle Remainder
+const toggleReminder=(id)=>{
+  // console.log(id)
+  setTasks(tasks.map((task)=>task.id === id?{...task,reminder:!task.reminder}:task))
+}
+
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={()=>setShowAddTask(!showAddTask)} 
+      showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length>0 ? <Tasks tasks={tasks} 
+      onDelete={deleteTask} 
+      onToggle={toggleReminder}/>:'No Tasks To Show'}
     </div>
   );
 }
