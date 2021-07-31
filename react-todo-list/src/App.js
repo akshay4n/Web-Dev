a402574f40599fc450b0b18e09d85d65e0644f14
akshay4n <akshay2701n@gmail.com>
2021-07-31 11:13:17 +0530

    functional-delete-btn

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -1,12 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './myComponents/Header';
+import {Footer} from './myComponents/Footer';
+import {Todos} from './myComponents/Todos';
+import React, { useState } from 'react';
 
 function App() {
+  const onDelete = (todo)=>{
+    console.log("Delete this task - ", todo.title);
+
+    setTodos(todos.filter((e)=>{
+      return e!==todo;
+    }))
+  }
+
+  const [todos, setTodos] = useState([
+    {
+      sno:1,
+      title:"Go to market",
+      desc:"Decription of going to the market"
+    },
+    {
+      sno:2,
+      title:"Go to mall",
+      desc:"Decription of going to the mall"
+    },
+    {
+      sno:3,
+      title:"Go to gym",
+      desc:"Decription of going to the gym"
+    }
+  ])
   return (
     <>
-      <Header/>
-      
+      <Header title='Todos List' searchBar={false}/>
+      <Todos todos={todos} onDelete={onDelete}/>
+      <Footer/>
     </>
   );
 }
---
+++ react-todo-list/src/myComponents/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Footer = () => {
+    let footerStyle = {
+        position:"absolute",
+        top:"100vh",
+        width:"100%",
+        // border:"2px solid red"
+    }
+    return (
+        <footer className='bg-dark text-light py-3'style={footerStyle}>
+            <p className="text-center">
+                Copyright &copy;
+                <a target='_blank' href='https://akshay4n.github.io/akshay4n/'>Akshay</a>
+            </p>
+        </footer>
+    )
+}
