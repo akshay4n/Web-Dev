a6f00639b08ac202424e9848e82d413983525f76
akshay4n <akshay2701n@gmail.com>
2021-07-31 21:50:49 +0530

    add and delete task(s)

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -2,37 +2,54 @@ import './App.css';
 import Header from './myComponents/Header';
 import {Footer} from './myComponents/Footer';
 import {Todos} from './myComponents/Todos';
-import React, { useState } from 'react';
+import { AddTodo } from './myComponents/AddTodo';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  let initTodo;
+  if(localStorage.getItem('todos') === null){
+    initTodo = [];
+  }
+  else{
+    initTodo = JSON.parse(localStorage.getItem('todos'));
+  }
   const onDelete = (todo)=>{
     console.log("Delete this task - ", todo.title);
 
     setTodos(todos.filter((e)=>{
       return e!==todo;
-    }))
+    }));
+    localStorage.setItem('todos', JSON.stringify(todos));
   }
 
-  const [todos, setTodos] = useState([
-    {
-      sno:1,
-      title:"Go to market",
-      desc:"Decription of going to the market"
-    },
-    {
-      sno:2,
-      title:"Go to mall",
-      desc:"Decription of going to the mall"
-    },
-    {
-      sno:3,
-      title:"Go to gym",
-      desc:"Decription of going to the gym"
+  const addTodo = (title,desc)=>{
+    console.log("Add this task",title,desc);
+    let sno;
+    if(todos.length===0){
+      sno=0;
     }
-  ])
+    else
+    {
+      sno = todos[todos.length-1].sno+1;
+    }
+    const myTodo={
+      sno:sno,
+      title:title,
+      desc:desc,
+    }
+    setTodos([...todos,myTodo]);
+    console.log(myTodo);
+  }
+
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos])
+
   return (
     <>
       <Header title='Todos List' searchBar={false}/>
+      <AddTodo addTodo={addTodo}/>
       <Todos todos={todos} onDelete={onDelete}/>
       <Footer/>
     </>
--- react-todo-list/src/myComponents/Footer.js
+++ react-todo-list/src/myComponents/Footer.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 export const Footer = () => {
-    let footerStyle = {
-        position:"absolute",
-        top:"100vh",
-        width:"100%",
-        // border:"2px solid red"
-    }
+    // let footerStyle = {
+    //     position:"relative",
+    //     top:"10vh",
+    //     width:"100%",
+    //     // border:"2px solid red"
+    // }
     return (
-        <footer className='bg-dark text-light py-3'style={footerStyle}>
+        <footer className='bg-dark text-light py-3' /*style={footerStyle}*/>
             <p className="text-center">
                 Copyright &copy;
                 <a target='_blank' href='https://akshay4n.github.io/akshay4n/'>Akshay</a>
